025b0df912676d056d937cccdbb97cbcd60216c9
XLsn0w <xlsn0wios@gmail.com>
2019-09-24 13:42:07 +0800

    add

---
+++ SwiftUI/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  SwiftUI
+//
+//  Created by mac on 2019/9/24.
+//  Copyright © 2019 XLsn0w. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ SwiftUI/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SwiftUI
+//
+//  Created by mac on 2019/9/24.
+//  Copyright © 2019 XLsn0w. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
